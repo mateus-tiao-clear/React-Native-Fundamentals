d122ce1fb1f16c404fb06960d77ba13a1fc8d685
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 18:51:18 -0300

    feat: understanding the props

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
+
+class App extends Component {
+  render() {
+    let nome = 'Matheus';
+    let img = 'https://sujeitoprogramador.com/steve.png';
+
+    return (
+      <View>
+        <Text>Olá Mundo!!!!</Text>
+        <Text>Meu Primeiro App</Text>
+        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+          Sujeito Programador
+        </Text>
+
+        <Image source={{uri: img}} style={{width: 300, height: 300}} />
+
+        <Text style={{fontSize: 30}}> {nome} </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
