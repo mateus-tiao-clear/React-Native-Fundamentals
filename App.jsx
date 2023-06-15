6c28a426719c3657987949949e9bc80ffdf1780e
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 19:25:21 -0300

    feat: creating project stopwatch app

--- App.jsx
+++ App.jsx
@@ -1,65 +1,138 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'; 
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity
+  } from 'react-native';
 
 class App extends Component{
+
   constructor(props){
     super(props);
     this.state = {
-      nome: '',
-      input: ''
+      numero: 0,
+      botao: 'VAI',
+      ultimo: null
     };
 
-    this.entrar = this.entrar.bind(this);
+    //Variavel do timer do relogio.
+    this.timer = null;
+
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
   }
 
-  entrar(){
-    if(this.state.input === ''){
-      alert('Digite seu nome!');
-      return;
+  vai(){
+
+    if(this.timer != null){
+      //Aqui vai parar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+
+      this.setState({botao: 'VAI'});
+    }else{
+
+      //Comeca girar o timer
+      this.timer = setInterval( ()=> {
+        this.setState({numero: this.state.numero + 0.1})
+      }, 100);
+
+      this.setState({botao: 'PARAR'});
     }
-    
-    this.setState({nome: 'Bem vindo: ' + this.state.input});
+
   }
 
+  limpar(){
+    if(this.timer != null){
+      //Aqui vai parar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({
+      ultimo: this.state.numero,
+      numero: 0,
+      botao: 'VAI'
+    })
+  }
 
   render(){
     return(
-      <View style={styles.container}>
+      <View style={styles.container}>  
 
-        <TextInput
-        style={styles.input}
-        placeholder="Digite seu nome?"
-        underlineColorAndroid="transparent"
-        onChangeText={ (texto) => this.setState({input: texto}) }
-        />
+      <Image
+      source={require('./src/assets/images/cronometro.png')}
+      style={styles.cronometro}
+      />
 
-        <Button title="Entrar" onPress={this.entrar} />
+      <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
 
-        <Text style={styles.texto}>{this.state.nome}</Text>
+      <View style={styles.btnArea}>
+
+        <TouchableOpacity style={styles.btn} onPress={this.vai}>
+          <Text style={styles.btnTexto}> {this.state.botao} </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
 
       </View>
+
+      <View style={styles.areaUltima}>
+          <Text style={styles.textoCorrida}>
+            {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) + 's' : ''}
+          </Text>
+      </View>
+
+
+      </View>    
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    alignItems:'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
-  input:{
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#222',
-    margin: 10,
+  timer:{
+    marginTop:-160,
+    color: '#FFF',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+  },
+  btnTexto:{
     fontSize: 20,
-    padding: 10,
+    fontWeight: 'bold',
+    color: '#00aeef'
   },
-  texto:{
-    textAlign: 'center',
-    fontSize: 25
+  areaUltima:{
+    marginTop: 40,
+  },
+  textoCorrida:{
+    fontSize:25,
+    fontStyle:'italic',
+    color: '#FFF'
   }
 });
-
-
 
 export default App;
