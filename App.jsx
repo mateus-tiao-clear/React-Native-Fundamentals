73c4a1dd75fccb34098dcc07d94ef50c9e5de816
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 18:55:28 -0300

    feat: Creating your components

--- App.jsx
+++ App.jsx
@@ -1,25 +1,43 @@
-import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native'; 
 
-class App extends Component {
-  render() {
+class App extends Component{
+  render(){
+
     let nome = 'Matheus';
-    let img = 'https://sujeitoprogramador.com/steve.png';
 
-    return (
+    return(
       <View>
         <Text>Olá Mundo!!!!</Text>
-        <Text>Meu Primeiro App</Text>
-        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+        <Text>Meu Primeiro App</Text>    
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
           Sujeito Programador
         </Text>
 
-        <Image source={{uri: img}} style={{width: 300, height: 300}} />
+        <Text style={{ fontSize: 30 }}> {nome} </Text>
 
-        <Text style={{fontSize: 30}}> {nome} </Text>
+        <Jobs largura={100} altura={200} fulano="Matheus" />
+
       </View>
     );
   }
 }
 
 export default App;
+
+class Jobs extends Component{
+  render(){
+
+    let img = 'https://sujeitoprogramador.com/steve.png';
+
+    return(
+      <View>
+        <Image 
+          source={{ uri: img }}
+          style={{ width: this.props.largura, height: this.props.altura }}
+      />
+      <Text> {this.props.fulano} </Text>
+     </View>
+    );
+  }
+}
