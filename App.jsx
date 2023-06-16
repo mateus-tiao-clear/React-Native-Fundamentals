4f96fe813977b8e207d9e696798c3caf2d4cbf91
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 21:46:52 -0300

    feat: SujeitoInsta

--- App.jsx
+++ App.jsx
@@ -1,21 +1,66 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Switch } from 'react-native';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Image, TouchableOpacity, FlatList } from 'react-native';
 
+import List from './src/components/List';
 export default function App() {
+ 
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nome: 'Tiao',
+      descricao: 'Ótimo dia! :)',
+      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
+      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
+      like: false,
+      qtdLike: 0
+    },
+    {
+      id: '2',
+      nome: 'Tiao',
+      descricao: 'Ótimo dia! :)',
+      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
+      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
+      like: false,
+      qtdLike: 3
+    }
 
-  const [status, setStatus] = useState(false)
-
+  ])
   return (
     <View style={styles.container}>
-      <Switch value={status} onValueChange={(newStatus) => setStatus(newStatus)} thumbColor={'blue'}/>
-      <Text style={{fontSize: 30}}>{status ? 'Ativo' : 'Inativo'}</Text>
-    </View>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image source={require('./src/assets/images/logo.png')} style={styles.logo} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={require('./src/assets/images/send.png')} style={styles.send} />
+        </TouchableOpacity>
+      </View>
+      <FlatList showsHorizontalScrollIndicator={true}
+        data={feed}
+        keyExtractor={ (item) => item.id }
+        renderItem={({ item }) => <List item={item}/>} />
+    </View >
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  header: {
+    marginTop: 30,
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1
+  },
+  send: {
+    width: 23,
+    height: 23
   }
+}
 });
