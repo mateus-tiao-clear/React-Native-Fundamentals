c38d54c6682e698fde892f761d276cff44614ea0
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 22:12:59 -0300

    feat: creating converter Coins

--- App.jsx
+++ App.jsx
@@ -1,49 +1,19 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard , ActivityIndicator} from 'react-native';
-
-import api_client from './src/config/api_client';
-
-import Movies from './src/components/Movies';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard, ActivityIndicator } from 'react-native';
+import Converter from './src/components/Converter';
 
 export default function App() {
-
-  const [filmes, setFilmes] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(async () => {
-    const response = await api_client.get('r-api/?api=filmes')
-    setFilmes(response.data)
-    setLoading(false)
-  }, [])
-
-  function renderData() {
-    if (!loading) {
-      return (
-        <View style={styles.container}>
-          <FlatList data={filmes} keyExtractor={item => item.id.toString()} renderItem={({ item }) => {
-            return <Movies data={item} />
-          }} />
-        </View >
-      )
-    } else {
-      return (
-        <View>
-          <ActivityIndicator></ActivityIndicator>
-        </View>
-      )
-    }
-  }
-
-  
   return (
-    <>
-      {renderData()}
-    </>
+    <View style={styles.container}>
+      <Converter moedaA='USD' moedaB='BRL'/>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
