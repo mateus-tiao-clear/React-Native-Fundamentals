09b311f12b030a4a47e677be7553daf9366b7e60
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 22:20:31 -0300

    feat: triggering animations

--- App.jsx
+++ App.jsx
@@ -3,39 +3,54 @@ import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, Toucha
 export default function App() {
   const [sizeAnimado, setSizeAnimado] = useState({
     width: new Animated.Value(150),
-    height: new Animated.Value(50)
+    height: new Animated.Value(50),
+    OpAnimada: new Animated.Value(0)
   })
 
-  Animated.loop(
+  function carregarGrafico() {
     Animated.sequence([
       Animated.timing(
-        sizeAnimado.width,
+        sizeAnimado.OpAnimada,
         {
-          toValue: 300,
-          duration: 2000
+          toValue: 1,
+          duration: 400
         }
       ),
       Animated.timing(
-        sizeAnimado.width,
+        sizeAnimado.height,
         {
-          toValue: 150,
-          duration: 2000
+          toValue: 200,
+          duration: 1000
         }
       )
-    ]),
-  ).start()
+    ]).start()
+  }
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{ width: sizeAnimado.width, height: sizeAnimado.height, backgroundColor: '#4169E1', justifyContent: 'center' }}>
-        <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }}>Carregando...</Text>
-      </Animated.View>
+      <Text>Vendas</Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={carregarGrafico}>
+          <Text style={{ fontSize: 25, color: '#FFFFFF' }}>Gerar Grafico</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+        <Animated.View style={{ width: sizeAnimado.width, height: sizeAnimado.height, backgroundColor: '#FF0000', justifyContent: 'center', opacity: sizeAnimado.OpAnimada }}>
+          <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }}>R$ 150.00</Text>
+        </Animated.View>
+      </View>
     </View >
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    height: 80,
+    backgroundColor: '#4169E1',
     justifyContent: 'center',
     alignItems: 'center'
   }
