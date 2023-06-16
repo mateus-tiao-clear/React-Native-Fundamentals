bea4650cd2cd29d507b661e6169dd1241f98d3c0
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 21:57:21 -0300

    feat: creating Modal

--- App.jsx
+++ App.jsx
@@ -1,44 +1,31 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, Switch, TextInput, Button, Image, TouchableOpacity, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard } from 'react-native';
 
-import List from './src/components/List';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Enter from './src/components/Enter';
+
 export default function App() {
- 
-  const [feed, setFeed] = useState([
-    {
-      id: '1',
-      nome: 'Tiao',
-      descricao: 'Ótimo dia! :)',
-      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
-      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
-      like: false,
-      qtdLike: 0
-    },
-    {
-      id: '2',
-      nome: 'Tiao',
-      descricao: 'Ótimo dia! :)',
-      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
-      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
-      like: false,
-      qtdLike: 3
-    }
 
-  ])
+  const [visible, setVisible] = useState(false)
+
+  const entrar = () => {
+    setVisible(true)
+  }
+
+  const sair = () => {
+    setVisible(false)
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity>
-          <Image source={require('./src/assets/images/logo.png')} style={styles.logo} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={require('./src/assets/images/send.png')} style={styles.send} />
-        </TouchableOpacity>
-      </View>
-      <FlatList showsHorizontalScrollIndicator={true}
-        data={feed}
-        keyExtractor={ (item) => item.id }
-        renderItem={({ item }) => <List item={item}/>} />
+      <Button title='Entrar' onPress={entrar} />
+
+      <Modal animationType='slide' visible={visible} transparent={true}>
+        <View style={{margin: 15, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Enter sair={sair} />
+        </View>
+      </Modal>
     </View >
   );
 }
@@ -46,21 +33,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  header: {
-    marginTop: 30,
-    height: 55,
-    backgroundColor: '#FFF',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: 5,
-    borderBottomWidth: 0.2,
-    shadowColor: '#000',
-    elevation: 1
-  },
-  send: {
-    width: 23,
-    height: 23
   }
-}
 });
