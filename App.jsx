e41c68602711bac69eb2b39a2301a900a1fdc63f
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 22:22:02 -0300

    feat: animation interpolate

--- App.jsx
+++ App.jsx
@@ -1,45 +1,25 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard, ActivityIndicator, Animated } from 'react-native';
+
 export default function App() {
-  const [sizeAnimado, setSizeAnimado] = useState({
-    width: new Animated.Value(150),
-    height: new Animated.Value(50),
-    OpAnimada: new Animated.Value(0)
+  const [animated, setAnimated] = useState({
+    width: new Animated.Value(0)
   })
 
-  function carregarGrafico() {
-    Animated.sequence([
-      Animated.timing(
-        sizeAnimado.OpAnimada,
-        {
-          toValue: 1,
-          duration: 400
-        }
-      ),
-      Animated.timing(
-        sizeAnimado.height,
-        {
-          toValue: 200,
-          duration: 1000
-        }
-      )
-    ]).start()
-  }
+  Animated.timing(
+    animated.width, {
+      toValue: 100,
+      duration: 5000
+    }
+  ).start()
 
+  let porcentAnimated = animated.width.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%']
+  })
   return (
     <View style={styles.container}>
-      <Text>Vendas</Text>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={carregarGrafico}>
-          <Text style={{ fontSize: 25, color: '#FFFFFF' }}>Gerar Grafico</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
-        <Animated.View style={{ width: sizeAnimado.width, height: sizeAnimado.height, backgroundColor: '#FF0000', justifyContent: 'center', opacity: sizeAnimado.OpAnimada }}>
-          <Text style={{ color: '#FFFFFF', fontSize: 22, textAlign: 'center' }}>R$ 150.00</Text>
-        </Animated.View>
-      </View>
+      <Animated.View style={{backgroundColor: '#4169E1', width: porcentAnimated, height: 25}}></Animated.View>
     </View >
   );
 }
@@ -47,11 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  header: {
-    height: 80,
-    backgroundColor: '#4169E1',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'flex-start'
   }
 });
