f09b48f4d434664312fb37a1369043829437b43c
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 21:18:28 -0300

    feat: Picker

--- App.jsx
+++ App.jsx
@@ -1,45 +1,52 @@
 import { useState } from 'react';
-import { StyleSheet, FlatList, View, Text } from 'react-native';
-import People from './src/components/People';
-
+import { StyleSheet, View, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
-  const [feed, setFeed] = useState([{
-    id: '1',
-    nome: 'tiao',
-    idade: 19,
-    email: 'tiao@gmail.com'
-  },
-  {
-    id: '2',
-    nome: 'João',
-    idade: 24,
-    email: 'joao@gmail.com'
-  },
-  {
-    id: '3',
-    nome: 'Pedro',
-    idade: 12,
-    email: 'pedro@gmail.com'
-  },
-  {
-    id: '4',
-    nome: 'Paulo',
-    idade: 35,
-    email: 'paulo@gmail.com'
-  }])
+ 
+  const [pizzas, setPizzas] = useState({
+    index: 0,
+    data: [
+      { key: 0, nome: 'Strogonoff', valor: 35.90 },
+      { key: 1, nome: 'Calabresa', valor: 37.90 },
+      { key: 2, nome: 'Portuguesa', valor: 32.90 }
+    ]
+  })
 
   return (
     <View style={styles.container}>
-      <FlatList keyExtractor={(item) => item.id} data={feed} renderItem={({ item }) => <People data={item} />}></FlatList>
+      <Text style={styles.logo}>Menu Pizza</Text>
+
+      <Picker style={styles.picker} selectedValue={pizzas.index} onValueChange={(itemValue, itemIndex) => setPizzas({
+        ...pizzas,
+        index: itemValue
+      })}>
+        {pizzas.data.map(pizza => {
+          return <Picker.Item key={pizza.key} value={pizza.key} label={pizza.nome} />
+        })}
+
+      </Picker>
+
+      <Text style={styles.pizzas}>Voce escolheu a pizza sabor {pizzas.data[pizzas.index].nome}</Text>
+      <Text style={styles.pizzas}>R$: {pizzas.data[pizzas.index].valor.toFixed(2)}</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  logo: {
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  pizzas: {
+    fontSize: 28,
+    marginTop: 15
+  },
+  picker: {
+    width: 200
+  }
 });
