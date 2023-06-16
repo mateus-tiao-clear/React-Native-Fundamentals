9e15bfca90e3204fc108fc64367c521f173511f2
MateusGomes04 <mateusgomesc04@gmail.com>
2023-06-15 21:22:32 -0300

    feat: Switch

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Slider from '@react-native-community/slider';
+import { StyleSheet, View, Text, Switch } from 'react-native';
 
 export default function App() {
-  const [valor, setValor] = useState(0)
+
+  const [status, setStatus] = useState(false)
 
   return (
     <View style={styles.container}>
-      <Slider style={styles.slider} minimumValue={0} maximumValue={100} value={0} onValueChange={(valor) => setValor(valor)}/>
-      <Text style={styles.texto}>Você tem {valor.toFixed(2)} kg</Text>
+      <Switch value={status} onValueChange={(newStatus) => setStatus(newStatus)} thumbColor={'blue'}/>
+      <Text style={{fontSize: 30}}>{status ? 'Ativo' : 'Inativo'}</Text>
     </View>
   );
 }
@@ -17,12 +17,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  
-  slider: {
-    width: '80%',
-  },
-  texto: {
-    fontSize: 20
   }
 });
